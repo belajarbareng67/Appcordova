efe9b2cef90924432899f3a58c2ef44ead29a841
belajarbareng67 <80940819+belajarbareng67@users.noreply.github.com>
2021-03-25 22:44:45 +0700

    Update db.php

--- www/db.php
+++ www/db.php
@@ -1,4 +1,4 @@
 <?php
  header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","root","","phonegab_demo") or die ("could not connect database");
+ $con = mysqli_connect("localhost","id16454557_cordova","PH6IOyov/lfWep(&","id16454557_dbcordova") or die ("could not connect database");
 ?>
